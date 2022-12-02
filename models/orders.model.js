49c2f2ee479855ddd63ac94ac180357a810b6ae7
dcladeira <daniel.ladeira@hotmail.com>
2022-12-02 10:52:29 -0300

    models

---
+++ models/orders.model.js
@@ -0,0 +1,28 @@
+import { model, Schema } from "mongoose";
+
+const orderSchema = new Schema(
+    {
+        products: [
+            {
+                product: {
+                    type: Schema.Types.ObjectId,
+                    ref: "Product"
+                },
+                amount: {
+                    type: Number
+                }
+            }
+        ],
+        salesDate: {
+            type: Date,
+            default: Date.now
+        }
+    }
+);
+
+const OrderModel = model("Order", orderSchema);
+
+export default OrderModel;
+
+
+
